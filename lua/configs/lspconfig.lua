e1ca40721c4b90ddaac39d06b89b5f8de0fc552e
kxue43 <xueke.kent@gmail.com>
2025-10-17 14:37:32 -0400

    feat: Set up some basic configs for `gopls`.

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,18 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls" }
+vim.lsp.config("gopls", {
+  settings = {
+    gopls = {
+      completeUnimported = true,
+      usePlaceholders = true,
+      analyses = {
+        unusedparams = true,
+      },
+    },
+  },
+})
+
+local servers = { "gopls", "lua_ls", "html", "cssls" }
 vim.lsp.enable(servers)
 
--- read :h vim.lsp.config for changing options of lsp servers 
+-- read :h vim.lsp.config for changing options of lsp servers
