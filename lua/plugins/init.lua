09ba7359ebfb456b350349d8f373fd7d340facc3
kxue43 <xueke.kent@gmail.com>
2025-10-17 19:44:00 -0400

    feat: `:Telescope live_grep` also searches hidden files.
      - But not gitignored files.

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -13,6 +13,42 @@ return {
     end,
   },
 
+  {
+    "nvim-telescope/telescope.nvim",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    cmd = "Telescope",
+    opts = function()
+      local opts = require("nvchad.configs.telescope")
+
+      opts.defaults.vimgrep_arguments = {
+        "rg",
+        "--color=never",
+        "--no-heading",
+        "--with-filename",
+        "--line-number",
+        "--column",
+        "--smart-case",
+        "--hidden",     -- Search hidden files
+        "--glob",
+        "!**/.git/*",   -- But exclude .git directory
+      }
+
+      opts.pickers = {
+        find_files = {
+          hidden = true, -- Include hidden files
+          -- no_ignore = true,  -- Uncomment to also include gitignored files
+        },
+        live_grep = {
+          additional_args = function()
+            return { "--hidden" }
+          end,
+        },
+      }
+
+      return opts
+    end,
+  },
+
   -- test new blink
   -- { import = "nvchad.blink.lazyspec" },
 
