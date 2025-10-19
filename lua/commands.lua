c6bfe7e8bd4a8c0c1c2f117e9e2e5b243fd1f527
kxue43 <xueke.kent@gmail.com>
2025-10-18 21:40:48 -0400

    feat: Port `showmd-vim-plugin` over as a key map.

--- lua/commands.lua
+++ lua/commands.lua
@@ -1,3 +1,3 @@
 vim.api.nvim_create_user_command("KMan", function(opts)
-  vim.cmd(":Man " .. opts.args .. " | only | NvimTreeToggle")
-end, { desc = "Open man page in new buffer as the only window.", nargs = "+" })
+  vim.cmd(":Man " .. opts.args .. " | only ")
+end, { desc = "Open man page in new buffer.", nargs = "+" })
--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,7 +1,9 @@
 require "nvchad.mappings"
 
+local map = vim.keymap.set
 
--- local map = vim.keymap.set
+map("n", "\\ll", function()
+  local curr_file = vim.fn.expand "%:p"
 
--- add yours here
-
+  vim.fn.system("toolkit-show-md " .. curr_file .. " >/dev/null 2>&1")
+end, { desc = "Convert and show the current Markdown file in browswer." })
