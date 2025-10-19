04447c18e290c885ac2b0159cc00f3c88f97559b
kxue43 <xueke.kent@gmail.com>
2025-10-19 15:07:26 -0400

    feat: Remap `<Tab>` and `<S-Tab>` in completion menus.

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,11 +1,51 @@
 return {
+  -- Turn on format on save.
   {
     "stevearc/conform.nvim",
-    event = 'BufWritePre', -- uncomment for format on save
+    event = "BufWritePre", -- uncomment for format on save
     opts = require "configs.conform",
   },
 
-  -- These are some examples, uncomment them if you want to see them work!
+  -- Change behavior of <Tab> and <S-Tab> when completion menu is visible.
+  -- Change <Tab> to cmp.confirm from cmp.select_next_item.
+  {
+    "hrsh7th/nvim-cmp",
+    opts = function()
+      local cmp = require "cmp"
+
+      local options = require "nvchad.configs.cmp"
+
+      local overrides = {
+        mapping = {
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              if not cmp.confirm { behavior = cmp.ConfirmBehavior.Insert, select = true } then
+                fallback()
+              end
+            elseif require("luasnip").expand_or_jumpable() then
+              require("luasnip").expand_or_jump()
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              fallback()
+            elseif require("luasnip").jumpable(-1) then
+              require("luasnip").jump(-1)
+            else
+              fallback()
+            end
+          end, { "i", "s" }),
+        },
+      }
+
+      return vim.tbl_deep_extend("force", options, overrides)
+    end,
+  },
+
+  -- Pull own LSP configs.
   {
     "neovim/nvim-lspconfig",
     config = function()
@@ -13,12 +53,13 @@ return {
     end,
   },
 
+  -- Make :Telescope live_grep also search hidden (dot) files.
   {
     "nvim-telescope/telescope.nvim",
     dependencies = { "nvim-treesitter/nvim-treesitter" },
     cmd = "Telescope",
     opts = function()
-      local opts = require("nvchad.configs.telescope")
+      local opts = require "nvchad.configs.telescope"
 
       opts.defaults.vimgrep_arguments = {
         "rg",
@@ -28,9 +69,9 @@ return {
         "--line-number",
         "--column",
         "--smart-case",
-        "--hidden",     -- Search hidden files
+        "--hidden", -- Search hidden files
         "--glob",
-        "!**/.git/*",   -- But exclude .git directory
+        "!**/.git/*", -- But exclude .git directory
       }
 
       opts.pickers = {
