45bca72da0c3b8cc3fd71c823107adec88e19aa7
kxue43 <xueke.kent@gmail.com>
2025-10-20 10:20:46 -0400

    feat: Define Gitsigns key maps.

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -90,16 +90,77 @@ return {
     end,
   },
 
-  -- test new blink
-  -- { import = "nvchad.blink.lazyspec" },
+  -- Gitsigns key maps.
+  {
+    "lewis6991/gitsigns.nvim",
+    event = "User FilePost",
+    opts = function()
+      local options = require "nvchad.configs.gitsigns"
 
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+      options.on_attach = function(bufnr)
+        local gitsigns = require "gitsigns"
+
+        local function map(mode, l, r, opts)
+          opts = opts or {}
+          opts.buffer = bufnr
+          vim.keymap.set(mode, l, r, opts)
+        end
+
+        -- Navigation
+        map("n", "]c", function()
+          if vim.wo.diff then
+            vim.cmd.normal { "]c", bang = true }
+          else
+            gitsigns.nav_hunk "next"
+          end
+        end)
+
+        map("n", "[c", function()
+          if vim.wo.diff then
+            vim.cmd.normal { "[c", bang = true }
+          else
+            gitsigns.nav_hunk "prev"
+          end
+        end)
+
+        -- Actions
+        map("n", "\\hs", gitsigns.stage_hunk)
+        map("n", "\\hr", gitsigns.reset_hunk)
+        map("n", "\\hp", gitsigns.preview_hunk)
+        map("n", "\\hi", gitsigns.preview_hunk_inline)
+
+        map("v", "\\hs", function()
+          gitsigns.stage_hunk { vim.fn.line ".", vim.fn.line "v" }
+        end)
+
+        map("v", "\\hr", function()
+          gitsigns.reset_hunk { vim.fn.line ".", vim.fn.line "v" }
+        end)
+
+        map("n", "\\hS", gitsigns.stage_buffer)
+        map("n", "\\hR", gitsigns.reset_buffer)
+
+        map("n", "\\hb", function()
+          gitsigns.blame_line { full = true }
+        end)
+
+        map("n", "\\hd", gitsigns.diffthis)
+
+        map("n", "\\hD", function()
+          gitsigns.diffthis "~"
+        end)
+
+        map("n", "\\hQ", function()
+          gitsigns.setqflist "all"
+        end)
+        map("n", "\\hq", gitsigns.setqflist)
+
+        -- Toggles
+        map("n", "\\tb", gitsigns.toggle_current_line_blame)
+        map("n", "\\tw", gitsigns.toggle_word_diff)
+      end
+
+      return options
+    end,
+  },
 }
