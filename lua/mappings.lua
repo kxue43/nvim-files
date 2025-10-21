f3bb17976094ee2e53838d888a873d8615c1e097
kxue43 <xueke.kent@gmail.com>
2025-10-21 16:41:53 -0400

    feat: Add key map for echoing current buffer path.

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -7,3 +7,7 @@ map("n", "\\ll", function()
 
   vim.fn.system("toolkit-show-md " .. curr_file .. " >/dev/null 2>&1")
 end, { desc = "Convert and show the current Markdown file in browswer." })
+
+map("n", "\\eb", function()
+  print(vim.fn.expand "%:p")
+end, { desc = "Echo full file path of the current buffer." })
