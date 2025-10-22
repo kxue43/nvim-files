41bee81eda486957a8b4e4fab90320580c22b357
kxue43 <xueke.kent@gmail.com>
2025-10-22 08:02:33 -0400

    feat: Use `blink.cmp`.

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -121,45 +121,6 @@ return {
     end,
   },
 
-  -- Change behavior of <Tab> and <S-Tab> when completion menu is visible.
-  -- Change <Tab> to cmp.confirm from cmp.select_next_item.
-  {
-    "hrsh7th/nvim-cmp",
-    opts = function()
-      local cmp = require "cmp"
-
-      local options = require "nvchad.configs.cmp"
-
-      local overrides = {
-        mapping = {
-          ["<Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              if not cmp.confirm { behavior = cmp.ConfirmBehavior.Insert, select = true } then
-                fallback()
-              end
-            elseif require("luasnip").expand_or_jumpable() then
-              require("luasnip").expand_or_jump()
-            else
-              fallback()
-            end
-          end, { "i", "s" }),
-
-          ["<S-Tab>"] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-              fallback()
-            elseif require("luasnip").jumpable(-1) then
-              require("luasnip").jump(-1)
-            else
-              fallback()
-            end
-          end, { "i", "s" }),
-        },
-      }
-
-      return vim.tbl_deep_extend("force", options, overrides)
-    end,
-  },
-
   -- Pull own LSP configs.
   {
     "neovim/nvim-lspconfig",
@@ -274,6 +235,27 @@ return {
         map("n", "\\tb", gitsigns.toggle_current_line_blame, { desc = "Toggle line blame." })
         map("n", "\\tw", gitsigns.toggle_word_diff, { desc = "Toggle word diff." })
       end
+
+      return options
+    end,
+  },
+
+  -- test new blink
+  { import = "nvchad.blink.lazyspec" },
+
+  {
+    "saghen/blink.cmp",
+    opts = function()
+      local options = require "nvchad.blink.config"
+
+      options.keymap = {
+        preset = "super-tab",
+        ["<CR>"] = { "accept", "fallback" },
+        ["<C-b>"] = { "scroll_documentation_up", "fallback" },
+        ["<C-f>"] = { "scroll_documentation_down", "fallback" },
+        ["<Down>"] = { "select_next", "snippet_forward", "fallback" },
+        ["<Up>"] = { "select_prev", "snippet_backward", "fallback" },
+      }
 
       return options
     end,
