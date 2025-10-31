dea711335478f08b7b890393fc313d89529775f0
kxue43 <xueke.kent@gmail.com>
2025-10-31 11:52:28 -0400

    feat: Always load which-key and never use buffer cmp.

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -16,6 +16,14 @@ return {
         ["<Up>"] = { "select_prev", "snippet_backward", "fallback" },
       }
 
+      options.enabled = function()
+        -- Disable completion in Markdown files.
+        return not vim.tbl_contains({ "markdown" }, vim.bo.filetype)
+      end
+
+      -- Compared to the NvChad default, 'buffer' is removed to disable text completion.
+      options.sources = { default = { "lsp", "snippets", "path" } }
+
       return options
     end,
   },
