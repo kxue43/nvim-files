3dbd3b6c8d027ba84c6ce3cd1e6c4af86bdec5da
kxue43 <xueke.kent@gmail.com>
2025-11-13 22:06:26 -0500

    feat: Open all folds by default.

--- lua/options.lua
+++ lua/options.lua
@@ -12,5 +12,6 @@ local o = vim.o
 -- Enable folding via treesitter.
 o.foldmethod = "expr"
 o.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+o.foldlevel = 99 -- Open all folds by default; Use zM to close all.
 
 -- o.cursorlineopt ='both' -- to enable cursorline!
