d48fa42926c794d394d67a12cfd5ada65d8c7e48
kxue43 <xueke.kent@gmail.com>
2026-03-11 11:31:55 -0400

    feat: Diagnostics always show source.

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,36 +1,50 @@
-require("nvchad.configs.lspconfig").defaults()
+local M = {}
 
--- Add own LSP configs here.
-vim.lsp.config("gopls", {
-  settings = {
-    gopls = {
-      completeUnimported = true,
-      usePlaceholders = false,
-      analyses = {
-        unusedparams = true,
-      },
-    },
-  },
-})
+M.defaults = function()
+  -- First use configs from Nvchad/Nvchad.
+  require("nvchad.configs.lspconfig").defaults()
 
-vim.lsp.config("basedpyright", {
-  settings = {
-    basedpyright = {
-      disableOrganizeImports = true,
-      analysis = {
-        autoSearchPaths = false,
-        typeCheckingMode = "basic",
-        useTypingExtensions = true,
-        inlayHints = {
-          callArgumentNames = false,
+  -- Always show diagnostics source.
+  -- vim.diagnostics.config performs table merge on config values.
+  vim.diagnostic.config {
+    virtual_text = { prefix = "", source = true },
+    float = { border = "single", source = true },
+  }
+
+  -- Add own LSP configs here.
+  vim.lsp.config("gopls", {
+    settings = {
+      gopls = {
+        completeUnimported = true,
+        usePlaceholders = false,
+        analyses = {
+          unusedparams = true,
         },
       },
     },
-  },
-})
+  })
 
--- Enable configured LSPs.
-local servers = { "gopls", "lua_ls", "bashls", "basedpyright", "ruff", "ts_ls", "stylua" }
-vim.lsp.enable(servers)
+  vim.lsp.config("basedpyright", {
+    settings = {
+      basedpyright = {
+        disableOrganizeImports = true,
+        analysis = {
+          autoSearchPaths = false,
+          typeCheckingMode = "basic",
+          useTypingExtensions = true,
+          inlayHints = {
+            callArgumentNames = false,
+          },
+        },
+      },
+    },
+  })
 
--- read :h vim.lsp.config for changing options of lsp servers
+  -- Enable configured LSPs.
+  local servers = { "gopls", "lua_ls", "bashls", "basedpyright", "ruff", "ts_ls", "stylua" }
+  vim.lsp.enable(servers)
+
+  -- read :h vim.lsp.config for changing options of lsp servers
+end
+
+return M
