994ad3ea3386f952f69a5c4f34a808eac46115ae
kxue43 <xueke.kent@gmail.com>
2026-03-28 21:30:43 -0400

    feat: Add `GeminiStart` command.

--- lua/commands.lua
+++ lua/commands.lua
@@ -1,3 +1,7 @@
 vim.api.nvim_create_user_command("KMan", function(opts)
   vim.cmd(":Man " .. opts.args .. " | only ")
 end, { desc = "Open man page in new buffer.", nargs = "+" })
+
+vim.api.nvim_create_user_command("GeminiStart", function()
+  vim.cmd ":Lazy load nvim-gemini-cli "
+end, { desc = "Manually load the nvim-gemini-cli plugin.", nargs = 0 })
